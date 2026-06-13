73f95818d6deb155a11c65aeda121af7795dd94c
Redacted <107178843+redactederror@users.noreply.github.com>
2026-06-13 19:33:24 +0100

    Update game mechanics and UI for snake game
    Refactor snake game logic and improve food spawning.

--- sites/pico.com/script.lua
+++ sites/pico.com/script.lua
@@ -1,15 +1,56 @@
 -- script.lua
 
+--------------------------------------------------
+-- CONFIG
+--------------------------------------------------
+
+tile_size = 8
+
+-- leave room for PicoWeb address bar
+playfield_y = 24
+
+playfield_w = 500
+playfield_h = 200
+
+grid_w = flr(playfield_w / tile_size)
+grid_h = flr((playfield_h - playfield_y) / tile_size)
+
+--------------------------------------------------
+-- GAME STATE
+--------------------------------------------------
+
 snake = {}
 food = {}
 
 dir_x = 1
 dir_y = 0
 
-move_timer = 0
-move_delay = 0.12
+next_dir_x = 1
+next_dir_y = 0
 
+move_timer = 0
+move_delay = 0.15
+
+score = 0
 game_over = false
+
+--------------------------------------------------
+-- HELPERS
+--------------------------------------------------
+
+function snake_contains(x,y)
+
+    for s in all(snake) do
+
+        if s.x == x and s.y == y then
+            return true
+        end
+
+    end
+
+    return false
+
+end
 
 --------------------------------------------------
 -- FOOD
@@ -17,27 +58,50 @@ game_over = false
 
 function spawn_food()
 
-    food.x = flr(rnd(50)) + 5
-    food.y = flr(rnd(25)) + 15
+    repeat
+
+        food.x =
+            flr(rnd(grid_w))
+
+        food.y =
+            flr(rnd(grid_h))
+
+    until not snake_contains(
+        food.x,
+        food.y
+    )
 
 end
 
 --------------------------------------------------
--- SITE INIT
+-- INIT
 --------------------------------------------------
 
 function _init_site()
 
+    local cx =
+        flr(grid_w / 2)
+
+    local cy =
+        flr(grid_h / 2)
+
     snake = {
-        {x=30,y=30},
-        {x=29,y=30},
-        {x=28,y=30}
+
+        {x=cx,y=cy},
+        {x=cx-1,y=cy},
+        {x=cx-2,y=cy}
+
     }
 
     dir_x = 1
     dir_y = 0
 
+    next_dir_x = 1
+    next_dir_y = 0
+
     move_timer = 0
+
+    score = 0
 
     game_over = false
 
@@ -52,49 +116,63 @@ end
 function handle_input()
 
     if btnp(0) and dir_x ~= 1 then
-        dir_x = -1
-        dir_y = 0
-    end
 
-    if btnp(1) and dir_x ~= -1 then
-        dir_x = 1
-        dir_y = 0
-    end
+        next_dir_x = -1
+        next_dir_y = 0
 
-    if btnp(2) and dir_y ~= 1 then
-        dir_x = 0
-        dir_y = -1
-    end
+    elseif btnp(1) and dir_x ~= -1 then
 
-    if btnp(3) and dir_y ~= -1 then
-        dir_x = 0
-        dir_y = 1
+        next_dir_x = 1
+        next_dir_y = 0
+
+    elseif btnp(2) and dir_y ~= 1 then
+
+        next_dir_x = 0
+        next_dir_y = -1
+
+    elseif btnp(3) and dir_y ~= -1 then
+
+        next_dir_x = 0
+        next_dir_y = 1
+
     end
 
 end
 
 --------------------------------------------------
--- MOVE SNAKE
+-- MOVE
 --------------------------------------------------
 
 function move_snake()
 
+    dir_x = next_dir_x
+    dir_y = next_dir_y
+
     local head = snake[1]
 
-    local nx = head.x + dir_x
-    local ny = head.y + dir_y
+    local nx =
+        head.x + dir_x
 
-    -- walls
+    local ny =
+        head.y + dir_y
 
-    if nx < 0 or nx > 63
-    or ny < 12 or ny > 63 then
+    --------------------------------------------------
+    -- WALLS
+    --------------------------------------------------
+
+    if nx < 0
+    or nx >= grid_w
+    or ny < 0
+    or ny >= grid_h then
 
         game_over = true
         return
 
     end
 
-    -- self collision
+    --------------------------------------------------
+    -- SELF COLLISION
+    --------------------------------------------------
 
     for seg in all(snake) do
 
@@ -108,21 +186,36 @@ function move_snake()
 
     end
 
+    --------------------------------------------------
+    -- ADD NEW HEAD
+    --------------------------------------------------
+
     add(
         snake,
         {
-            x=nx,
-            y=ny
+            x = nx,
+            y = ny
         },
         1
     )
 
-    -- food
+    --------------------------------------------------
+    -- FOOD
+    --------------------------------------------------
 
     if nx == food.x
     and ny == food.y then
 
+        score += 1
+
         spawn_food()
+
+        -- tiny speed increase
+        move_delay =
+            max(
+                0.05,
+                move_delay - 0.005
+            )
 
     else
 
@@ -136,7 +229,7 @@ function move_snake()
 end
 
 --------------------------------------------------
--- SITE UPDATE
+-- UPDATE
 --------------------------------------------------
 
 function _update_site()
@@ -144,7 +237,11 @@ function _update_site()
     if game_over then
 
         if btnp(4) then
+
+            move_delay = 0.15
+
             _init_site()
+
         end
 
         return
@@ -166,63 +263,139 @@ function _update_site()
 end
 
 --------------------------------------------------
--- SITE DRAW
+-- DRAW TILE
+--------------------------------------------------
+
+function draw_tile(
+    gx,
+    gy,
+    col
+)
+
+    local px =
+        gx * tile_size
+
+    local py =
+        playfield_y +
+        gy * tile_size
+
+    rectfill(
+        px,
+        py,
+        px + tile_size - 1,
+        py + tile_size - 1,
+        col
+    )
+
+end
+
+--------------------------------------------------
+-- DRAW
 --------------------------------------------------
 
 function _draw_site()
 
     cls(1)
 
+    --------------------------------------------------
+    -- UI
+    --------------------------------------------------
+
     print(
         "PicoSnake",
-        2,
-        2,
+        4,
+        playfield_y,
         11
     )
 
     print(
-        "Length: "..#snake,
-        2,
-        9,
+        "Score: "..score,
+        90,
+        playfield_y,
         7
     )
 
-    -- food
+    --------------------------------------------------
+    -- FOOD
+    --------------------------------------------------
 
-    pset(
+    draw_tile(
         food.x,
         food.y,
         8
     )
 
-    -- snake
+    --------------------------------------------------
+    -- SNAKE
+    --------------------------------------------------
 
-    for seg in all(snake) do
+    for i=1,#snake do
 
-        pset(
-            seg.x,
-            seg.y,
-            11
-        )
+        local seg =
+            snake[i]
+
+        if i == 1 then
+
+            draw_tile(
+                seg.x,
+                seg.y,
+                11
+            )
+
+        else
+
+            draw_tile(
+                seg.x,
+                seg.y,
+                10
+            )
+
+        end
 
     end
 
+    --------------------------------------------------
+    -- GAME OVER
+    --------------------------------------------------
+
     if game_over then
 
-        print(
-            "GAME OVER",
-            20,
-            30,
+        rectfill(
+            120,
+            70,
+            370,
+            120,
+            0
+        )
+
+        rect(
+            120,
+            70,
+            370,
+            120,
             8
         )
 
         print(
-            "PRESS X",
-            20,
-            40,
+            "GAME OVER",
+            200,
+            85,
+            8
+        )
+
+        print(
+            "PRESS X TO RESTART",
+            170,
+            100,
             7
         )
 
     end
 
 end
+
+--------------------------------------------------
+-- START
+--------------------------------------------------
+
+_init_site()
