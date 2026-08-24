9de0ea26eb29ec08a89883cdd05955ab2394cbca
Redacted <107178843+redactederror@users.noreply.github.com>
2026-08-25 00:37:02 +0100

    Add _draw_site function to display PicoWeb info

---
+++ sites/pico/script.lua
@@ -0,0 +1,52 @@
+
+function _draw_site()
+
+    print("Pico",10,20,7)
+
+    print("Welcome to PicoWeb.",10,34,6)
+
+    print(
+        "PicoWeb lets you run Picotron",
+        10,
+        52,
+        7
+    )
+
+    print(
+        "carts directly from the Pico Web.",
+        10,
+        62,
+        7
+    )
+
+    print("How it works",10,84,11)
+
+    print(
+        "Type a site's name into the",
+        10,
+        98,
+        7
+    )
+
+    print(
+        "address bar to open it.",
+        10,
+        108,
+        7
+    )
+
+    print(
+        "Sites run inside PicoWeb without",
+        10,
+        122,
+        7
+    )
+
+    print(
+        "needing to be downloaded or installed.",
+        10,
+        132,
+        7
+    )
+
+end
